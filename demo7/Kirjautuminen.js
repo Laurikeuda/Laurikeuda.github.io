ff7d788c4c7977bc37cde43f763d04e812917915
Laurikeuda <99252543+Laurikeuda@users.noreply.github.com>
2022-03-07 09:13:54 +0200

    kirjaautimisnes

--- demo7/Kirjautuminen.js
+++ demo7/Kirjautuminen.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut() {
     let kirjautunut = 'kylla';
     if(kirjautunut === 'kylla'){
-        document.getElementById('tervetulo_teksti').textContent += 'Lauri';
+        document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
     }
 }
 
 function kirjaudu() {
-    localStorage.setItem("nimi", "Lauri");
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
 }
